d0e1d872e348e79509bcf1ac2dc2df77c54e18ba
Ivan <solarmaltsev@yandex.ru>
2023-05-27 16:52:28 +0300

    sprint-9

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/PostNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/PostNotFoundException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
 public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(String message){
+    public PostNotFoundException(String message) {
         super(message);
     }
 }
