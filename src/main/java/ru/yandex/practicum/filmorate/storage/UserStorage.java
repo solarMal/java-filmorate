34a24adefa9686dbe47a33f1b09aaa7a4e3ef639
Ivan <solarmaltsev@yandex.ru>
2023-07-19 17:51:53 +0300

    sprint 11

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.storage;
 import org.springframework.http.ResponseEntity;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.Set;
-
 public interface UserStorage {
     ResponseEntity<?> createUser(User user);
 
