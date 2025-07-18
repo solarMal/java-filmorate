a9a80e55ec21797f8854251193d9f1e9eda86e7f
Ivan <solarmaltsev@yandex.ru>
2025-07-18 13:08:03 +0300

    добавил перенос строки в интерфейсах

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -5,8 +5,12 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Set;
 
 public interface FilmStorage {
+
     Film createFilm(Film film);
+
     Film updateFilm(Film film);
+
     Set<Film> getAllFilms();
+
     Film getFilmById(long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -7,8 +7,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     User createUser(User user);
+
     User updateUser(User user);
+
     User getUserById(long id);
+
     List<User> getUsers();
 }
