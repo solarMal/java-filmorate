87777ef85aa899ada9509947c723655622692267
Ivan <solarmaltsev@yandex.ru>
2025-10-08 17:13:23 +0300

    добавил enum(genre,mpa,friendStatus)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,9 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -22,6 +24,8 @@ public class Film {
 
     @Builder.Default
     private Set<Long> likes = new HashSet<>();
+    private Set<Genre> genre = new HashSet<>();
+    private Set<MPA> mpa = new HashSet<>();
 
     public Film(long id, String name, String description, LocalDate releaseDate, int duration) {
         this.id = id;
@@ -31,4 +35,14 @@ public class Film {
         this.duration = duration;
     }
 
+    public Film(long id, String name, String description, LocalDate releaseDate, int duration, Set<Genre> genre
+            , Set<MPA> mpa) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+        this.genre = genre;
+        this.mpa = mpa;
+    }
 }
