2d9f8cd38724a66a0be8577f3dcec242e4e174af
Ivan <solarmaltsev@yandex.ru>
2025-10-09 16:45:06 +0300

    добавил ER-диаграмму базы данных

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,9 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Getter
